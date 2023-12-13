b3d3326081a70335c67ab961d490856d1cdd95e5
Razeema R C <122087518+razeemarc@users.noreply.github.com>
2023-12-13 06:38:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<html lang="en">
+  <head>
+  <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
+     
+    <link rel="stylesheet" href="css/style.css" />
+  
+
+    <title>Career Navigator</title>
+    
+  </head>
+  <body>
+    <div class="menu-bar">
+      <h1 class="logo">Car<span>Nav.</span></h1>
+      <ul>
+        <li><a href="#">Career Selection<i class="fas fa-caret-down"></i></a>
+          <div class="dropdown-menu">
+            <ul>
+              <li><a href="engineer.html">Engineering</a></li>
+              <li><a href="#">Medical<i class="fas fa-caret-right"></i></a>
+              
+                 <div class="dropdown-menu-1">
+                <ul>
+                  <li><a href="doctor.html">Doctor</a></li>
+                  <li><a href="nurse.html">Nurse</a></li>
+                  
+                </ul>
+              </div>
+            </li></li>
+              <li>
+                <a href="teacher.html">Teacher </i></a>
+              
+              </li>
+              
+              <li><a href="lawyer.html">Lawyer</a></li>
+              <li><a href="architect.html">Architect</a></li>
+              <li><a href="journalist.html">Journalist</a></li>
+
+
+            </ul>
+          </div></li>
+        <li><a href="#">Graduates <i class="fas fa-caret-down"></i></a>  <div class="dropdown-menu">
+                <ul>
+                  <li><a href="engineer1.html">Engineer</a></li>
+                  <li><a href="doctor1.html">Doctor</a></li>
+                  <li><a href="lawyer1.html">Lawyer</a></li>
+                  <li><a href="architect1.html">Architect</a></li>
+              <li><a href="journalist1.html">Journalist</a></li>
+              <li><a href="nurse1.html">Nurse</a></li>
+
+
+
+
+                </ul>
+              </div>
+        </li></li>
+        <li><a href="#"> Exams <i class="fas fa-caret-down"></i></a>
+
+            <div class="dropdown-menu">
+                <ul>
+                  <li><a href="banking.html">Banking</a></li>
+                  <li><a href="defense.html">Defense</a></li>
+                  <li><a href="teaching.html">Teaching</a></li>
+                  <li><a href="railway.html">Railway</a></li>
+                  <li><a href="ssc.html">SSC</a></li>
+                  <li><a href="all.html">All Exams</a></li>
+
+
+
+                </ul>
+              </div>
+        </li></li>
+
+        <li><a href="contact.html">Contact us</a></li>
+      </ul>
+      
+    </div>
+   
+
+     <div id="slider">
+        <figure>
+            <img src="img/Designer.gif" alt="">
+            <img src="img/Designer (1).gif" alt="">
+            <img src="img/Designer (2).gif" alt="">
+            <img src="img/Designer (1).gif" alt="">
+            <img src="img/Designer (2).gif" alt="">
+        </figure>
+     </div>
+     <script type="text/javascript">
+    (function(d, m){
+        var kommunicateSettings = 
+            {"appId":"2fc2d19783818ba44816bda2f9b0668d8","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        window.kommunicate = m; m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+</script>
+  </body>
+</html>
---
+++ index1.php
@@ -0,0 +1,138 @@
+<?php include('server.php') ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Webpage Design</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
+
+.bg {
+  /* The image used */
+  background-image: url(".jpg");
+
+  /* Full height */
+  height: 100%; 
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+</head>
+
+
+
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">CarNav</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#about">ABOUT</a></li>
+                    <li><a href="#service">SERVICE</a></li>
+                    
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+            </div>
+            <!--
+
+            <div class="search">
+                <input class="srch" type="search" name="" placeholder="Type To text">
+                <a href="#"> <button class="btn">Search</button></a>
+            </div>-->
+
+        </div> 
+        <div class="content">
+            <h1>Career <span>Navigator</span></h1>
+        
+            <p class="par">        Welcome to our career guidance website! We are here to help you navigate your professional journey and achieve your career goals. Whether you're a student exploring different career paths, a recent graduate starting your job search, or a seasoned professional looking to make a career change, our website provides comprehensive resources and expert advice to support you every step of the way. Our goal is to empower you to make informed decisions and build a fulfilling and successful career. Let's get started!
+            </p>
+
+                <button class="cn"><a href="register.php">JOIN US</a></button>
+<!--login
+     <div class="form">
+                    <h2>Login Here</h2>
+                    <input type="email" name="email" placeholder="Enter Email Here">
+                    <input type="password" name="" placeholder="Enter Password Here">
+                    <button class="btnn"><a href="#">Login</a></button>
+
+                    <p class="link">Don't have an account<br>
+                    <a href="#">Sign up </a> here</a></p>
+                    <p class="liw">Log in with</p>
+
+                    <div class="icons">
+                        <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-skype"></ion-icon></a>
+                    </div>
+
+                </div>
+            -->
+                    </div>
+                    
+                </div>
+                <section class="about" id="about">
+                    <div class="about">
+                <div class="content">
+            <h1>About <span>Us</span></h1>
+            <p class="par">   Welcome to our career guidance website! We are dedicated to helping individuals explore career options, identify their strengths and interests, and make informed decisions about their career paths.
+
+Our team of career experts and counselors are here to assist you in achieving your career goals. We provide a variety of services, including career assessments, job search strategies, resume writing, interview preparation, and much more.
+
+Our goal is to provide you with the knowledge and resources you need to make informed decisions about your career. We believe that everyone deserves to find a career that aligns with their passions and interests, and we are here to help you achieve that.
+
+We understand that choosing a career path can be a daunting task, and that's why we offer personalized guidance to help you navigate the process. Whether you're a recent graduate, looking to switch careers, or simply exploring your options, we are here to support you every step of the way.
+
+At our career guidance website, we are committed to helping you succeed in your career. We believe that with the right tools and guidance, anyone can achieve their career aspirations. Let us help you achieve yours!
+            </p>
+
+                </div>
+    </section>
+        </div>
+        <section class="service" id="service">
+                    <div class="service">
+                <div class="content">
+            <h1><span>Service</span></h1>
+            <p class="par">  Here is some sample content for a service page on a career guidance website:
+
+---
+
+ we understand that choosing the right career path can be a daunting task. With so many options available, it's easy to feel overwhelmed and unsure about where to start. That's where our career guidance services come in.
+
+Our team of experienced career counselors is here to help you navigate the complex world of career choices and find the path that's right for you. We offer a range of services designed to support you at every stage of your career journey, from career exploration to job search strategies and more.
+
+Our services include:
+
+1. Career assessment: We offer a comprehensive assessment that evaluates your strengths, interests, and values to help you identify career options that align with your unique profile.
+
+2. Career exploration: We provide resources and guidance to help you explore various career paths and industries, giving you a better understanding of the opportunities available to you.
+
+3. Career planning: Our counselors work with you to create a personalized career plan that outlines your goals and the steps you need to take to achieve them.
+
+4. Job search strategies: We offer guidance on job search techniques, including networking, resume writing, and interview preparation, to help you succeed in your job search.
+
+5. Career coaching: We provide ongoing coaching and support to help you stay on track and achieve your career goals.
+
+Our team of counselors has years of experience working with individuals from diverse backgrounds and industries. We are committed to providing personalized guidance and support to help you make informed decisions about your career.
+
+Contact us today to schedule a consultation and take the first step toward a fulfilling and successful career.
+            </p>
+
+                </div>
+    </section>
+        </div>
+    </div>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+</body>
+</html>
